f547a8f4d63ad4747ee5920f4526b0b97c0c2e88
unknown <chc238u20@CHC238W20.Instruction.network.bcc>
2014-05-01 20:02:38 -0700

    Upload my junk

---
+++ Zimmer Project#3/project3.js
@@ -0,0 +1,29 @@
+$('#scooby').hide();
+$('#shaggy').hide();
+$('#fred').hide();
+$('#daphne').hide();
+$('#velma').hide();
+
+$('li>a').click(function(){
+	hideAll();
+	
+	var divToShow = $(this).attr('target');
+	$('#' + divToShow).show();
+	
+});
+
+$('li').mouseenter(function(){
+	$(this).addClass('hilite');
+	});
+
+	$('li').mouseleave(function(){
+		$(this).removeClass('hilite');
+		});
+		
+function hideAll(){
+$('#scooby').hide();
+$('#shaggy').hide();
+$('#fred').hide();
+$('#daphne').hide();
+$('#velma').hide();
+}
